3853101d32b72f3f423bda88f15ff98224738d26
Ajay kharche <ajay.kharche@newvision-software.com>
2026-07-06 19:31:55 +0530

    Initial commit

--- Demotest/test1.spec.js
+++ Demotest/test1.spec.js
@@ -9,7 +9,7 @@ test('Open application without logging in', async ({ page }) => {
 
     await page.goto('https://www.saucedemo.com/inventory.html');
 
-    await expect(page).toHaveURL(/inventory/)
+    //await expect(page).toHaveURL(/inventory/)
     //page.waitForTimeout(2000);
 
 });
---
+++ Demotest/testjsonData.spec.ts
@@ -0,0 +1,5 @@
+const str: string = "madamnb";
+
+const reversed = str.split("").reverse().join("");
+
+console.log(str==reversed ? 'palindrome':'not palindrome');
